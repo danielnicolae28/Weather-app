6e5f80c14ce01179b6a2760bc83c6b181bbc312e
danielnicolae28 <danielnicolae286@gmail.com>
2024-04-09 21:30:26 +0300

    demo effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { useEffect,useState } from 'react';
 import './App.css'
 
+
+interface DummyType {
+  coord:{
+lon:number,
+lat:number
+  };
+  city:string;
+ 
+}
+
+
 function App() {
-  const [count, setCount] = useState(0)
+
+   const [data,setData]=useState<DummyType[]>([])
+
+
+  const url = 'https://open-weather13.p.rapidapi.com/city/landon/%7Blang%7D';
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': 'f2ab028663msh36324debed3f59cp19e7e3jsn408de536ee8e',
+      'X-RapidAPI-Host': 'open-weather13.p.rapidapi.com'
+    }
+  };
+
+      useEffect( async ()=>{
+        const response = await fetch(url,options);
+        const result = await response.json();
+
+        setData(result)
+        console.log(result)
+},[])
 
   return (
+
+
+
+
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+   {
+    data.map((data)=>(
+      <h1>{data.city}</h1>
+    ))
+   }
     </>
   )
 }
